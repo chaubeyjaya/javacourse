4a55fcd8b1b093370606d27e626d2a0292177460
chaubeyjaya <jayachaubey03@gmail.com>
2026-03-18 20:25:25 +0530

    fistr changes

---
+++ Basic/Basic.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Basic{
+    public static void main(String[] args) {
+        System.err.println("Hello");
+        int a = 3;
+        int b  = 4;
+
+        //sum of two number taking the user input
+        System.out.println("sum of two number " + a+b);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int r = sc.nextInt();
+        System.out.println("Enter the Scond number");
+        int j = sc.nextInt();
+        System.out.println("Sum of enter two number "+(r+j));
+
+//all type of datatype
+        int num = 12;
+        String name ="Jaya";
+        Boolean ishuman = true;
+        double num2 = 1.12;
+
+
+    }
+}
