fc1258a3aafb53066daa3b205f5fbab8b7a23cfe
chaubeyjaya <jayachaubey03@gmail.com>
2026-03-18 20:37:06 +0530

    factorial and Areaof circle

---
+++ Basic/AreaOfCircle.java
@@ -0,0 +1,11 @@
+public class AreaOfCircle{
+
+    public static void main(String[] args) {
+        double pi = 3.14;
+        int r = 4;
+        double area = pi*r*r;
+       System.out.println("Area of circle "+ (area));
+    }
+
+ 
+}
